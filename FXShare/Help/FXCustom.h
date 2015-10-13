d795ccf0915ce19ee7b4f4c3e4cbd29cf56a278d
Zeacone <andreboot42@gmail.com>
2015-10-13 23:32:42 +0800

    Working on custom titlebar.

--- FXShare/Help/FXCustom.h
+++ FXShare/Help/FXCustom.h
@@ -8,6 +8,40 @@
 
 #import <Cocoa/Cocoa.h>
 
+///----------------------------------------
+///  Custom titlebar of window
+///----------------------------------------
+
 @interface FXPanel : NSPanel
+
+@end
+
+
+///----------------------------------------
+///  Custom titlebar of window
+///----------------------------------------
+
+@interface FXTitleBar : NSView
+
+/**
+ *  Build three buttons.
+ */
+@property (strong) NSButton *closeButton, *minimizeButton, *zoomButton;
+@property (strong) NSImage *backgroundPattern;
+@property (strong) NSBezierPath *outline;
+@property (strong) NSTrackingArea *buttonArea;
+
+- (void)updateOutline;
+
+@end
+
+///----------------------------------------
+///  Custom window with custom titlebar.
+///----------------------------------------
+
+@interface FXWindow : NSWindow
+
+@property (readonly) NSColor *backgroundPattern;
+@property (strong) FXTitleBar *windowTitleBar;
 
 @end
